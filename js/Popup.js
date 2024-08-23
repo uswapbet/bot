624f2f1dd7c170c0a6ab544ed43a218c73e8bf9e
uswap-app <132031468+uswap-app@users.noreply.github.com>
2024-08-23 08:04:55 +0530

    Add files via upload

--- js/Popup.js
+++ js/Popup.js
@@ -29,18 +29,22 @@ class Popup {
         document.getElementById('clear-inputs-btn').addEventListener('click', () => {
             this.clearInputs();
         });
-        // Check Draw (Placeholder for functionality)
+        // Check Draw
         document.getElementById('check-draw-btn').addEventListener('click', () => {
             this.checkDraw();
+        });
+        // Listen for changes in the transaction ID input
+        document.getElementById('input-transaction-id').addEventListener('input', () => {
+            this.clearRelatedFields();
         });
     }
 
     async show() {
-        this.popupPanel.style.display = 'block';
+        this.popupPanel.classList.add('show-popup');
     }
 
     async hide() {
-        this.popupPanel.style.display = 'none';
+        this.popupPanel.classList.remove('show-popup');
     }
 
     async clearInputs() {
@@ -48,45 +52,54 @@ class Popup {
         document.getElementById('input-transaction-id').value = '';
         document.getElementById('input-hash-id').value = '';
         document.getElementById('input-draw-number').value = '';
-        document.getElementById('popup-process-info').value = '';
+        this.resetPopupInfo();
+    }
+
+    clearRelatedFields() {
+        document.getElementById('input-block-id').value = '';
+        document.getElementById('input-hash-id').value = '';
+        document.getElementById('input-draw-number').value = '';
+        this.resetPopupInfo();
     }
 
     async checkDraw() {
-        await this.getTransactionInfo();
+        const transactionIdEl = document.getElementById('input-transaction-id').value;
+        if (transactionIdEl) {
+            await this.getTransactionInfo();
+        } else {
+            this.resetPopupInfo();
+        }
     }
 
     async getTransactionInfo() {
         try {
             const transactionId = document.getElementById('input-transaction-id').value;
-            const result  = await this.ssc.getTransactionInfo(transactionId);
-            
-            if(result) {
-                const {
-                    blockNumber,                    
-                    payload,
-                    hash,
-                    logs
-                } = result;
+            const result = await this.ssc.getTransactionInfo(transactionId);
+
+            if (result) {
+                const { blockNumber, payload, hash, logs } = result;
 
                 const parsedPayload = JSON.parse(payload);
                 const parsedLogs = JSON.parse(logs);
+
                 if (parsedLogs && !parsedLogs.errors && parsedLogs.events?.length > 0) {
                     const fetchData = await this.tokenFetcher.fetchTokenSettings();
-                    if(fetchData.length > 0) {
+                    if (fetchData.length > 0) {
                         const accountChecker = new AccountChecker(this.ssc, this.tokenSymbol, fetchData);
                         const accData = await accountChecker.betTokenAccount();
-                        if(accData !== null) {
-                            if(parsedPayload.symbol === this.tokenSymbol && parsedPayload.to === accData) { 
+                        if (accData !== null) {
+                            if (parsedPayload.symbol === this.tokenSymbol && parsedPayload.to === accData) {
                                 const calculateNumber = new CalculateNumber();
                                 const calcNumber = await calculateNumber.calcNumber(blockNumber, transactionId, hash);
-                                if(calcNumber !== null) {
+                                if (calcNumber !== null) {
                                     document.getElementById('input-block-id').value = blockNumber;
                                     document.getElementById('input-hash-id').value = hash;
                                     document.getElementById('input-draw-number').value = calcNumber;
+                                    this.resetPopupInfo();
                                 } else {
                                     let errMsg = "Draw number generator makes error, please try again...";
                                     await this.popUpErrorInfo(errMsg);
-                                } 
+                                }
                             } else {
                                 let errMsg = "Token name or Sender name mismatch, please check again...";
                                 await this.popUpErrorInfo(errMsg);
@@ -95,19 +108,19 @@ class Popup {
                             let errMsg = "Sender name generator makes error, please try again...";
                             await this.popUpErrorInfo(errMsg);
                         }
-                    }  else {
+                    } else {
                         let errMsg = "Encounter an error, please try again...";
                         await this.popUpErrorInfo(errMsg);
                     }
                 } else {
                     let errMsg = "Encounter an hive-engine error, please try again...";
                     await this.popUpErrorInfo(errMsg);
-                } 
+                }
             } else {
                 let errMsg = "Encounter an hive-engine error, please try again...";
                 await this.popUpErrorInfo(errMsg);
             }
-            
+
         } catch (error) {
             console.log("Error at getTransactionInfo():", error);
             let errMsg = "Encounter an error, please try again...";
@@ -117,10 +130,15 @@ class Popup {
 
     async popUpErrorInfo(errMsg, errorStatus = true) {
         try {
-            await this.messageViewer.showMessageWithFade('popup-process-info', 
+            await this.messageViewer.showMessageWithFade('popup-process-info',
                 errMsg, 'red', false, errorStatus);
         } catch (error) {
             console.log("Error at popUpErrorInfo():", error);
         }
+    }
+
+    resetPopupInfo() {
+        document.getElementById('popup-process-info').style.visibility = 'hidden';
+        document.getElementById('popup-process-info').style.opacity = '0';
     }
 }
